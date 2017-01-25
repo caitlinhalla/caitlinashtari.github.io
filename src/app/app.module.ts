b8d52ad652611fa1ff73b1b27fdc757dcaee988e
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-25 15:39:30 -0800

    set up file structure and material.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
